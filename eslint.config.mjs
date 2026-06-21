87933a204f1fea127109b9b7341e43d533490df2
jd-oviedo <274356266+jd-oviedo@users.noreply.github.com.>
2026-06-21 14:57:33 +0000

    Add eslint-config-next, downgrade non-blocking rules

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "warn",
+      "react/no-unescaped-entities": "warn",
+    },
+  },
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+];
+export default eslintConfig;
