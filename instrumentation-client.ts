cff69a321bf5f85c92166bf9b8dc0e05d66c0912
jd-oviedo <274356266+jd-oviedo@users.noreply.github.com.>
2026-06-22 17:07:06 +0000

    chore: update next, eslint-config-next, supabase-js, posthog-js

---
+++ instrumentation-client.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://140962e2aa77c0f18993162b757cefd5@o4511610070499328.ingest.us.sentry.io/4511610070695936",
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
